0b29439a3bd0cc9f44ea352c700b8c1a5abfecd8
Archie-C <archie_l_carpenter@hotmail.com>
2026-03-27 10:44:47 +0100

    Fixed ruff failing

--- src/ehrdrec/data/loaders/base.py
+++ src/ehrdrec/data/loaders/base.py
@@ -1,9 +1,12 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+
 import polars as pl
 
+
 class BaseLoader(ABC):
-    def __init__(self, source):
+    def __init__(self, source: str) -> None:
         self.source = source
-    
+
+    @abstractmethod
     def load(self) -> pl.DataFrame:
         raise NotImplementedError
